8a7c4c377906212db0c808d21eca898c04c81913
99jinmo <99jinmo@gmail.com>
2025-04-07 10:40:02 +0900

    Init Visual Studio
    첫 시작

---
+++ OOPConsoleProject/OOPConsoleProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
